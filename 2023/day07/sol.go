24c862ec28f4a0e5ec32a0b4a9411d0eae4e2af2
ToTheMax <21971014+ToTheMax@users.noreply.github.com>
2023-12-07 17:55:10 +0100

    Day 7

--- 2023/day07/sol.go
+++ 2023/day07/sol.go
@@ -8,96 +8,83 @@ import (
 	"strings"
 )
 
-type Card struct {
-	name  string
-	score int
-	count int
-}
-
 type Hand struct {
-	rank  int
-	score int
+	str string
 	bid   int
+	rank  int
 }
 
-func cardScore(char rune) int {
-	ascii_num := int(char - '0')
-	if ascii_num >= 0 && ascii_num <= 9 {
-		return ascii_num
-	} else {
-		if char == 'T' {
-			return 10
-		} else if char == 'J' {
-			return 11
-		} else if char == 'Q' {
-			return 12
-		} else if char == 'K' {
-			return 13
-		} else if char == 'A' {
-			return 14
+func cardScore(char rune, level int) int {
+	if level == 1{
+		return strings.Index("23456789TJQKA", string(char))
+	} else{
+		return strings.Index("J23456789TQKA", string(char))
+	}
+}
+
+
+func handRank(str string, bid int, level int) Hand {
+	cardCounts := make([]int, 13)
+	for _, char := range str {
+		cardCounts[cardScore(char, level)]++
+	}
+	
+	jokers := 0
+	if level == 2 {
+		// Replace Jokers with 0
+		jokers = cardCounts[0]
+		cardCounts[0] = 0
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(cardCounts)))
+
+	if level == 2 {
+		cardCounts[0] += jokers
+	}
+	rankings := map[int][]int{
+		7: {5,0}, // Five of a kind
+		6: {4,1}, // Four of a kind
+		5: {3,2}, // Full House
+		4: {3,1}, // Three of a kind
+		3: {2,2}, // Two Pairs
+		2: {2,1}, // Single Pair
+		1: {1,1}, // High Card
+	}
+
+	for rank, counts := range rankings{
+		if cardCounts[0] == counts[0] && cardCounts[1] == counts[1]{
+			return Hand{str, bid, rank}
 		}
 	}
-	return -1
+	return Hand{str, bid, -1}
 }
 
-func handScore(hand string, bid int) Hand {
-	cardsMap := map[string]*Card{}
-	for _, char := range hand {
-		name := string(char)
-		if _, ok := cardsMap[name]; ok {
-			cardsMap[name].count++
+func sortHands(hands []Hand, level int) {
+	sort.Slice(hands[:], func(i, j int) bool {
+		// First compare ranks, then string
+		if hands[i].rank != hands[j].rank {
+			return hands[i].rank < hands[j].rank
 		} else {
-			cardsMap[name] = &Card{
-				name:  string(char),
-				score: cardScore(char),
-				count: 1,
+			for k := range hands[i].str {
+				if hands[i].str[k] != hands[j].str[k] {
+					cardi := cardScore(rune(hands[i].str[k]),level)
+					cardj := cardScore(rune(hands[j].str[k]),level)
+					return cardi < cardj	
+				}
 			}
 		}
-	}
-
-	cards := make([]Card, 0)
-	for _, card := range cardsMap {
-		cards = append(cards, *card)
-	}
-
-	sort.Slice(cards[:], func(i, j int) bool {
-		// First compare counts, then score
-		cardi := cards[i].count*100 + cards[i].score
-		cardj := cards[j].count*100 + cards[j].score
-		return cardi > cardj
+		return false
 	})
+}
 
-	// Check for Five of a kind
-	if cards[0].count == 5 {
-		return Hand{6, cards[0].score, bid}
-	}
-	// Check for Four of a kind
-	if cards[0].count == 4 {
-		return Hand{5, 10*cards[0].score + cards[1].score, bid}
-	}
 
-	// Check for Full House
-	if cards[0].count == 3 && cards[1].count == 2 {
-		return Hand{4, 10*cards[0].score + cards[1].score, bid}
+func calcScore(hands []Hand, level int) int {
+	sortHands(hands, level)
+	totalWinnings := 0
+	for i, hand := range hands {
+		totalWinnings += (i + 1) * hand.bid
 	}
-
-	// Check for Three of a kind
-	if cards[0].count == 3 {
-		return Hand{3, 100*cards[0].score + 10*cards[1].score + cards[2].score, bid}
-	}
-
-	// Check for Two Pairs
-	if cards[0].count == 2 && cards[1].count == 2 {
-		return Hand{2, 100*cards[0].score + 10*cards[1].score + cards[2].score, bid}
-	}
-
-	// Check for Single Pair
-	if cards[0].count == 2 {
-		return Hand{1, 1000*cards[0].score + 100*cards[1].score + 10*cards[2].score + cards[3].score, bid}
-	}
-
-	// Check for High Card
-	return Hand{0, 10000*cards[0].score + 1000*cards[1].score + 100*cards[2].score + 10*cards[3].score + cards[4].score, bid}
+	return totalWinnings
 }
 
 func main() {
@@ -109,22 +96,15 @@ func main() {
 		splittedLine := strings.Split(line, " ")
 		hand := splittedLine[0]
 		bid, _ := strconv.Atoi(splittedLine[1])
-		hands[i] = handScore(hand, bid)
-		// fmt.Println(handScore(hand, bid))
+		hands[i] = handRank(hand, bid, 1)
 	}
 
-	sort.Slice(hands[:], func(i, j int) bool {
-		// First compare ranks, then score
-		cardi := hands[i].rank*100000 + hands[i].score
-		cardj := hands[j].rank*100000 + hands[j].score
-		return cardi < cardj
-	})
-
-	totalWinnings := 0
-	for i, hand := range hands {
-		fmt.Println(i+1, "*", hand.bid)
-		totalWinnings += (i + 1) * hand.bid
-
+	fmt.Println("Sol 1:", calcScore(hands, 1))
+	
+	for i, hand := range hands{
+		hands[i] = handRank(hand.str, hand.bid, 2)
 	}
-	fmt.Println("Sol 1", totalWinnings)
+
+	fmt.Println("Sol 2:", calcScore(hands, 2))
+	
 }
