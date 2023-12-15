5c3bba699372470a4d6ce2e22afe4a1afbfd53a6
Max <21971014+ToTheMax@users.noreply.github.com>
2023-12-15 11:16:56 +0100

    day15

--- 2023/day15/sol.go
+++ 2023/day15/sol.go
@@ -3,15 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
+
+type Lens struct {
+	focalLength int
+	label       string
+	index       int
+}
 
 func hash(step string) int {
 	curValue := 0
 	for _, char := range step {
-		curValue += int(char)
-		curValue *= 17
-		curValue %= 256
+		curValue = (curValue + int(char)) * 17 % 256
 	}
 	return curValue
 }
@@ -20,11 +25,52 @@ func main() {
 	input, _ := os.ReadFile("in.txt")
 	steps := strings.Split(string(input), ",")
 
-	scoreP1 := 0
-	for _, step := range steps {
-		scoreP1 += hash(step)
-	}
-
 	// Part 1
-	fmt.Println("Sol 1:", scoreP1)
+	sumP1 := 0
+	for _, step := range steps {
+		sumP1 += hash(step)
+	}
+	fmt.Println("Sol 1:", sumP1)
+
+	// Part 2
+	sumP2 := 0
+	buckets := make([]map[string]Lens, 256)
+	for i := range buckets {
+		buckets[i] = make(map[string]Lens)
+	}
+	for _, step := range steps {
+		// Lens removal
+		if step[len(step)-1] == '-' {
+			label := step[:len(step)-1]
+			bucket := buckets[hash(label)]
+			if removedLens, ok := bucket[label]; ok {
+				delete(bucket, label)
+				for _, l := range bucket {
+					if l.index > removedLens.index {
+						bucket[l.label] = Lens{l.focalLength, l.label, l.index - 1}
+					}
+				}
+			}
+		}
+
+		// Lens addition
+		if step[len(step)-2] == '=' {
+			split := strings.Split(step, "=")
+			label := split[0]
+			value, _ := strconv.Atoi(split[1])
+
+			bucket := buckets[hash(label)]
+			if lens, ok := bucket[label]; ok {
+				bucket[label] = Lens{value, label, lens.index}
+			} else {
+				bucket[label] = Lens{value, label, len(bucket)}
+			}
+		}
+	}
+	for b, bucket := range buckets {
+		for _, lens := range bucket {
+			sumP2 += (b + 1) * (lens.index + 1) * lens.focalLength
+		}
+	}
+	fmt.Println("Sol 2:", sumP2)
 }
