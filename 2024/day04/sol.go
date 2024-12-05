931329d8e4f795bc6f6757cf27ae632d32b05dda
ToTheMax <21971014+ToTheMax@users.noreply.github.com>
2024-12-05 15:43:09 +0100

    day 4 cleanup

--- 2024/day04/sol.go
+++ 2024/day04/sol.go
@@ -6,28 +6,23 @@ import (
 	"strings"
 )
 
-func inBounds(lines []string, x, y int) bool {
-	return x >= 0 && x < len(lines[0]) && y >= 0 && y < len(lines)
-}
-
 func searchXmas(lines []string, x, y int) int {
-
 	// Check all directions
-	xmas_checks := [][][]int{
-		{{x, y}, {x, y + 1}, {x, y + 2}, {x, y + 3}},             // Horizontal
-		{{x, y}, {x + 1, y}, {x + 2, y}, {x + 3, y}},             // Vertical
-		{{x, y}, {x + 1, y + 1}, {x + 2, y + 2}, {x + 3, y + 3}}, // Descending diagonal
-		{{x, y}, {x - 1, y + 1}, {x - 2, y + 2}, {x - 3, y + 3}}, // Ascending diagonal
+	words := []string{}
+	if x+3 < len(lines[y]) {
+		words = append(words, fmt.Sprintf("%c%c%c%c", lines[y][x], lines[y][x+1], lines[y][x+2], lines[y][x+3]))
+	}
+	if y+3 < len(lines) {
+		words = append(words, fmt.Sprintf("%c%c%c%c", lines[y][x], lines[y+1][x], lines[y+2][x], lines[y+3][x]))
+	}
+	if y+3 < len(lines) && x+3 < len(lines[y]) {
+		words = append(words, fmt.Sprintf("%c%c%c%c", lines[y][x], lines[y+1][x+1], lines[y+2][x+2], lines[y+3][x+3]))
+	}
+	if y+3 < len(lines) && x-3 >= 0 {
+		words = append(words, fmt.Sprintf("%c%c%c%c", lines[y][x], lines[y+1][x-1], lines[y+2][x-2], lines[y+3][x-3]))
 	}
 	count := 0
-	for _, check := range xmas_checks {
-		word := ""
-		for _, coord := range check {
-			if !inBounds(lines, coord[0], coord[1]) {
-				break
-			}
-			word += string(lines[coord[1]][coord[0]])
-		}
+	for _, word := range words {
 		if word == "XMAS" || word == "SAMX" {
 			count++
 		}
@@ -36,27 +31,16 @@ func searchXmas(lines []string, x, y int) int {
 }
 
 func searchMas(lines []string, x, y int) int {
-
-	// Check if there is A in the center
-	if y < len(lines)-1 && x < len(lines[0])-1 && lines[y+1][x+1] != 'A' {
+	// Check bounds and center A
+	if x > len(lines[0])-3 || y > len(lines)-3 {
 		return 0
 	}
 
-	// Check if there are M and S in the corners
-	M_count := 0
-	S_count := 0
-	mas_check := [][]int{{x, y}, {x + 2, y}, {x, y + 2}, {x + 2, y + 2}}
-	for _, coord := range mas_check {
-		if !inBounds(lines, coord[0], coord[1]) {
-			return 0
-		}
-		if lines[coord[1]][coord[0]] == 'M' {
-			M_count++
-		} else if lines[coord[1]][coord[0]] == 'S' {
-			S_count++
-		}
-	}
-	if M_count == 2 && S_count == 2 {
+	// Check MAS in diagonals
+	word1 := fmt.Sprintf("%c%c%c", lines[y][x], lines[y+1][x+1], lines[y+2][x+2])
+	word2 := fmt.Sprintf("%c%c%c", lines[y][x+2], lines[y+1][x+1], lines[y+2][x])
+
+	if (word1 == "MAS" || word1 == "SAM") && (word2 == "MAS" || word2 == "SAM") {
 		return 1
 	}
 	return 0
@@ -77,5 +61,5 @@ func main() {
 	}
 
 	fmt.Println(countP1)
-	fmt.Println(countP2) // 1900 was too high
+	fmt.Println(countP2)
 }
