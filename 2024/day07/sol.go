c5114168759620a5d8353a2bc8d2df7133e6fa92
ToTheMax <21971014+ToTheMax@users.noreply.github.com>
2024-12-07 16:08:17 +0100

    day 7 cleanup

--- 2024/day07/sol.go
+++ 2024/day07/sol.go
@@ -15,27 +15,24 @@ const (
 	Concat
 )
 
-func solveEquation(equation []int, operators []Operator, value int, target int) int {
-	solves := 0
+func solveEquation(equation []int, operators []Operator, value int, target int) bool {
 	if len(equation) == 0 {
-		if value == target {
-			return 1
-		}
-		return 0
+		return value == target
 	}
 
+	solved := false
 	for _, operator := range operators {
 		switch operator {
 		case Add:
-			solves += solveEquation(equation[1:], operators, value+equation[0], target)
+			solved = solveEquation(equation[1:], operators, value+equation[0], target)
 		case Multiply:
-			solves += solveEquation(equation[1:], operators, value*equation[0], target)
+			solved = solved || solveEquation(equation[1:], operators, value*equation[0], target)
 		case Concat:
 			value, _ = strconv.Atoi(fmt.Sprintf("%d%d", value, equation[0]))
-			solves += solveEquation(equation[1:], operators, value, target)
+			solved = solved || solveEquation(equation[1:], operators, value, target)
 		}
 	}
-	return solves
+	return solved
 }
 
 func main() {
@@ -53,10 +50,10 @@ func main() {
 		for i, s := range splitted {
 			ints[i], _ = strconv.Atoi(s)
 		}
-		if solveEquation(ints[1:], []Operator{Add, Multiply}, ints[0], target) > 0 {
+		if solveEquation(ints[1:], []Operator{Add, Multiply}, ints[0], target) {
 			solP1 += target
 		}
-		if solveEquation(ints[1:], []Operator{Add, Multiply, Concat}, ints[0], target) > 0 {
+		if solveEquation(ints[1:], []Operator{Add, Multiply, Concat}, ints[0], target) {
 			solP2 += target
 		}
 	}
