be91198310e63c805be81665fe9f0ac8beadc8d1
ToTheMax <21971014+ToTheMax@users.noreply.github.com>
2024-12-18 01:40:15 +0100

    day 14 p2

--- 2024/day14/sol.go
+++ 2024/day14/sol.go
@@ -23,43 +23,73 @@ func mod(a, b int) int {
 	return ((a % b) + b) % b
 }
 
-func main() {
-	input, _ := os.ReadFile("in.txt")
-	lines := strings.Split(string(input), "\n")
-
-	width := 101
-	height := 103
-
+func parseRobots(lines []string) []Robot {
 	robots := make([]Robot, len(lines))
 	for i, line := range lines {
 		var p Pos
 		var v Velocity
 		fmt.Sscanf(line, "p=%d,%d v=%d,%d", &p.x, &p.y, &v.x, &v.y)
-
 		robots[i] = Robot{pos: p, velocity: v}
 	}
+	return robots
+}
 
-	for second := 0; second < 100; second++ {
+func solve(robots []Robot, width, height int, part int) int {
+	maxLocations := len(robots)
+
+	for second := 0; second < 100000; second++ {
 		for i, robot := range robots {
 			robots[i].pos.x = mod(robot.pos.x+robot.velocity.x, width)
 			robots[i].pos.y = mod(robot.pos.y+robot.velocity.y, height)
 		}
-	}
 
-	// Count how many robots are in each quadrant
-	horCenter := width / 2
-	verCenter := height / 2
-	quadrant := make([]int, 4)
-	for _, robot := range robots {
-		if robot.pos.x < horCenter && robot.pos.y < verCenter {
-			quadrant[0]++
-		} else if robot.pos.x < horCenter && robot.pos.y > verCenter {
-			quadrant[1]++
-		} else if robot.pos.x > horCenter && robot.pos.y < verCenter {
-			quadrant[2]++
-		} else if robot.pos.x > horCenter && robot.pos.y > verCenter {
-			quadrant[3]++
+		if part == 1 && second == 100 {
+			quads := make([]int, 4)
+			for _, robot := range robots {
+				if robot.pos.x < width/2 && robot.pos.y < height/2 {
+					quads[0]++
+				} else if robot.pos.x < width/2 && robot.pos.y > height/2 {
+					quads[1]++
+				} else if robot.pos.x > width/2 && robot.pos.y < height/2 {
+					quads[2]++
+				} else if robot.pos.x > width/2 && robot.pos.y > height/2 {
+					quads[3]++
+				}
+			}
+			return quads[0] * quads[1] * quads[2] * quads[3]
+		}
+		if part == 2 {
+			locations := make(map[Pos]bool)
+			for _, robot := range robots {
+				locations[robot.pos] = true
+			}
+			if len(locations) == maxLocations {
+				printMap(locations, width, height)
+				return second + 1
+			}
 		}
 	}
-	fmt.Println("Sol 1:", quadrant[0]*quadrant[1]*quadrant[2]*quadrant[3])
+	return -1
+}
+
+// Print the robots in the map, robot an R, the rest a dot
+func printMap(locations map[Pos]bool, width, height int) {
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			if locations[Pos{x, y}] {
+				fmt.Print("R")
+			} else {
+				fmt.Print(".")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func main() {
+	input, _ := os.ReadFile("in.txt")
+	lines := strings.Split(string(input), "\n")
+
+	fmt.Println("Sol 1:", solve(parseRobots(lines), 101, 103, 1))
+	fmt.Println("Sol 2:", solve(parseRobots(lines), 101, 103, 2))
 }
